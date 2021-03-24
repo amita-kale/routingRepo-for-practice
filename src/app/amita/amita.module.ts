0dd527281459274c0c7835868614ade005d90216
Amita kale <amitakale297@gmail.com>
2021-03-24 12:05:59 +0530

    amita module create here

---
+++ src/app/amita/amita.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AmitaRoutingModule } from './amita-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AmitaRoutingModule
+  ]
+})
+export class AmitaModule { }
